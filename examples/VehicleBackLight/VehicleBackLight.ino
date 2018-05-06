c7ecaf46b126a82e911c2f473ed3aa49a61c072b
Christoph Tack <prog.send@gmail.com>
2018-05-06 21:51:32 +0200

    Implementing infinite scrolling text
    Signed-off-by: Christoph Tack <prog.send@gmail.com>

--- examples/VehicleBackLight/VehicleBackLight.ino
+++ examples/VehicleBackLight/VehicleBackLight.ino
@@ -12,7 +12,7 @@
 #define F2(progmem_ptr) (const __FlashStringHelper *)progmem_ptr
 
 seven_eighty_rg matrix(4,5,6,10, false);
-const char str[] PROGMEM = "Adafruit 16x32 RGB LED Matrix";
+const char str[] /*PROGMEM*/ = "# HOU AFSTAND # KEEP DISTANCE ";
 typedef struct
 {
     byte centerX;
@@ -23,29 +23,29 @@ typedef struct
     unsigned long ultime;
 }GROWING_CIRCLE;
 
-GROWING_CIRCLE gc1={4,3,0,3,seven_eighty_rg::ORANGE,0};
-GROWING_CIRCLE gc2={4,3,2,3,seven_eighty_rg::RED,0};
-GROWING_CIRCLE gc3={75,3,0,3,seven_eighty_rg::ORANGE,0};
-GROWING_CIRCLE gc4={75,3,2,3,seven_eighty_rg::RED,0};
+GROWING_CIRCLE gc[4]={ {4,3,0,3,seven_eighty_rg::ORANGE,0}, {4,3,2,3,seven_eighty_rg::RED,0},
+                       {75,3,0,3,seven_eighty_rg::ORANGE,0}, {75,3,2,3,seven_eighty_rg::RED,0} };
 
 void setup() {
+    Serial.begin(9600);
     matrix.begin();
     matrix.setTextWrap(false); // Allow text to run off right edge
     matrix.setTextSize(1);
     matrix.setTextColor(matrix.ORANGE);
-//    matrix.drawLine(0,0,79,6,matrix.RED);
+    //    matrix.drawLine(0,0,79,6,matrix.RED);
     //matrix.setFont(&TomThumb);
     //matrix.setFont(&Tiny3x3a2pt7b);
-//    matrix.setFont(&Picopixel);
-//    matrix.drawChar(0, 7, 'a',matrix.ORANGE, matrix.BLACK, 1);
+    //    matrix.setFont(&Picopixel);
+    //    matrix.drawChar(0, 7, 'a',matrix.ORANGE, matrix.BLACK, 1);
 }
 
 void loop()
 {
-    growingCircles(&gc1);
-    growingCircles(&gc2);
-    growingCircles(&gc3);
-    growingCircles(&gc4);
+    infiniteScrollingText(str);
+    for(byte i=0;i<4; i++)
+    {
+        growingCircles(&gc[i]);
+    }
 }
 
 void growingCircles(GROWING_CIRCLE* gc)
@@ -53,9 +53,31 @@ void growingCircles(GROWING_CIRCLE* gc)
     if(millis()>gc->ultime+100)
     {
         gc->ultime=millis();
-        matrix.drawCircle(gc->centerX, gc->centerY, gc->radius, seven_eighty_rg::BLACK);
         gc->radius = gc->radius > gc->maxRadius ? 0 : gc->radius+1;
         matrix.drawCircle(gc->centerX, gc->centerY, gc->radius, gc->color);
+    }
+}
+
+void infiniteScrollingText(char* string)
+{
+    static unsigned long ulTime=0;
+    static const byte CHAR_WIDTH=6;
+    static const byte CURSORX_OFFSET=9;
+    static int cursorX=CHAR_WIDTH-1;
+    static int offset=0;
+    int msgLen=strlen(string);
+    if(millis()>ulTime+50)
+    {
+        ulTime=millis();
+        matrix.fillScreen(seven_eighty_rg::BLACK);
+        matrix.setCursor(CURSORX_OFFSET+cursorX,0);
+        cursorX=(cursorX==0?CHAR_WIDTH-1:cursorX-1);
+        matrix.print(string+offset);
+        if(cursorX==CHAR_WIDTH-1)
+        {
+            offset=(offset==msgLen-1?0:offset+1);
+        }
+        matrix.print(string);
     }
 }
 
